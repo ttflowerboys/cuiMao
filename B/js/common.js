e06a93b26018b6a7942472375e6e4a78d10d7039
smartLe <ttflowerboys@gmail.com>
2017-06-25 16:42:34 +0800

    Add rele and cooperation FED design

---
+++ B/js/common.js
@@ -0,0 +1,34 @@
+$(function(){
+  var $el, leftPos, newWidth;
+      $mainNav2 = $("#example-two");
+
+  $mainNav2.append("<li id='magic-line-two'></li>");
+
+  var $magicLineTwo = $("#magic-line-two");
+
+  $magicLineTwo
+      .width($(".current_page_item_two").width())
+      // .height($mainNav2.height())
+      .css("left", $(".current_page_item_two a").position().left)
+      .data("origLeft", $(".current_page_item_two a").position().left)
+      .data("origWidth", $magicLineTwo.width())
+      .data("origColor", $(".current_page_item_two a").attr("rel"));
+
+  $("#example-two a").hover(function() {
+      $el = $(this);
+      leftPos = $el.position().left;
+      newWidth = $el.parent().width();
+      $magicLineTwo.stop().animate({
+          left: leftPos,
+          width: newWidth,
+          backgroundColor: $el.attr("rel")
+      },100)
+  // }, function() {
+  //     $magicLineTwo.stop().animate({
+  //         left: $magicLineTwo.data("origLeft"),
+  //         width: $magicLineTwo.data("origWidth"),
+  //         backgroundColor: $magicLineTwo.data("origColor")
+  //     },100);
+  });
+
+})
